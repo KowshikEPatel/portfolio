d7976aafb3c7167aea9a8559c3468165fdd54543
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-07-18 13:29:22 +0530

    removing wraning to build

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -21,7 +21,7 @@ export default function Projects() {
                 <div className='cardtitle'>{projects[0]['projectname']}</div>
                 <div >{projects[0]['projectdescription']}</div>
                 <div style={{display:'flex'}}>{projects[0]['projectlibraries'].map((e,index)=>{ return <div style={{border:"2px solid green",fontSize:'0.7rem',fontWeight:'510' ,margin:'3px',padding:'3px'}} key={index}>{e}</div> })}</div>
-                <div><a href=''><i class="fas fa-link"></i></a></div>
+                <div><a href='https://friendly-feynman-57301c.netlify.app'><i class="fas fa-link"></i></a></div>
             </div>
         </div>
     )
