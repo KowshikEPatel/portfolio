dea873711f1d6b5062ae5a7ae6b5ddcb28e2a056
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-07-20 11:18:39 +0530

    projects and background sections updated

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -5,8 +5,10 @@ export default function Background() {
     return (
         <div className='background'>
             <div>Background</div>
-            <div style={{margin:'0px 10px 0px 10px'}}>A Nanoscience and Nanotechnology masters graduate from K U Leuven, (Belgium) who is aspiring to build a career in full stack development after getting certified from Guvi Geeks network of full stack development (MERN stack)</div>
-            <div><a href='https://drive.google.com/file/d/1Yvy2z7GA-4zRinEAorCeG6I15CfMBVNL/view?usp=sharing' className='btn btn-success'>Resume</a></div>
+            <div style={{margin:'0px 10px 0px 10px'}}>A Nanoscience and Nanotechnology masters graduate from K U Leuven, (Belgium) who is aspiring to build a career in full stack development after getting certified from Guvi Geeks network of full stack development (MERN stack)
+            <div style={{margin:'10px'}}><a href='https://drive.google.com/file/d/1Yvy2z7GA-4zRinEAorCeG6I15CfMBVNL/view?usp=sharing' className='btn btn-success'>Resume</a></div>
+            </div>
+            
         </div>
     )
 }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -5,24 +5,43 @@ export default function Projects() {
     const projects = [
                         {
                             "projectname":"microURL",
-                            "projectdescription": "An easy to use URL shortening tool with a graph of number of clicks on each of the links ",
-                            "projectlibraries":["React router dom","React context API", "ChartJS", "Gmail API",""],
+                            "projectdescription": "An easy to use URL shortening tool with a graph of number of clicks on each of the links created by current user.",
+                            "projectlibraries":["React router dom","React context API", "ChartJS", "Express","Gmail API", "MongoDB NodeJS driver"],
                         },
                         {
-                            "projectname":"",
-                            "projectdescription":"",
-                            "projectlibraries":[ ],
+                            "projectname":"microURL",
+                            "projectdescription": "An easy to use URL shortening tool with a graph of number of clicks on each of the links created by current user.",
+                            "projectlibraries":["React router dom","React context API", "ChartJS", "Express","Gmail API", "MongoDB NodeJS driver"],
                         },
+                        {
+                            "projectname":"microURL",
+                            "projectdescription": "An easy to use URL shortening tool with a graph of number of clicks on each of the links created by current user.",
+                            "projectlibraries":["React router dom","React context API", "ChartJS", "Express","Gmail API", "MongoDB NodeJS driver"],
+                        },
+                        {
+                            "projectname":"microURL",
+                            "projectdescription": "An easy to use URL shortening tool with a graph of number of clicks on each of the links created by current user.",
+                            "projectlibraries":["React router dom","React context API", "ChartJS", "Express","Gmail API", "MongoDB NodeJS driver"],
+                        }
                     ]
     return (
         <div className='projects'>
             <div>Projects</div>
-            <div className='cardelement'>
-                <div className='cardtitle'>{projects[0]['projectname']}</div>
-                <div >{projects[0]['projectdescription']}</div>
-                <div style={{display:'flex'}}>{projects[0]['projectlibraries'].map((e,index)=>{ return <div style={{border:"2px solid green",fontSize:'0.7rem',fontWeight:'510' ,margin:'3px',padding:'3px'}} key={index}>{e}</div> })}</div>
-                <div><a href='https://friendly-feynman-57301c.netlify.app'><i class="fas fa-link"></i></a></div>
+            <div className='cardelement' style={{margin:'10px',padding:'10px',fontSize:'1.0rem'}}>
+                { projects.map((element,index)=>{
+                    return <div key={index}>
+                                <div className='cardtitle'>{element['projectname']}</div>
+                                <div >{element['projectdescription']}</div>
+                                <div style={{display:'flex'}}>{element['projectlibraries'].map((e,index)=>{ return <div style={{border:"2px solid green",fontSize:'0.7rem',fontWeight:'510' ,margin:'3px',padding:'3px'}} key={index}>{e}</div> })}</div>
+                                <div style={{display:'flex'}}>
+                                    <div style={{margin:'0 5px 0 5px'}}><a href='https://github.com/KowshikEPatel/urlshortnerfe'><i className="fab fa-github"></i></a></div>
+                                    <div style={{margin:'0 5px 0 5px'}}><a href='https://friendly-feynman-57301c.netlify.app'><i className="fas fa-link"></i></a></div>
+                                </div>
+                            </div>
+                })}
+                  <a className='otherprojects' href='https://github.com/KowshikEPatel?tab=repositories'> Check out my other projects on github</a>
             </div>
+            
         </div>
     )
 }
