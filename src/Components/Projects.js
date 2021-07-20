caa40c83fb86356aa6d661fa7a986c87c43c14e3
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-07-20 23:37:39 +0530

    project list edit

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -28,17 +28,16 @@ export default function Projects() {
         <div className='projects'>
             <div>Projects</div>
             <div className='cardelement' style={{margin:'10px',padding:'10px',fontSize:'1.0rem'}}>
-                { projects.map((element,index)=>{
-                    return <div key={index}>
-                                <div className='cardtitle'>{element['projectname']}</div>
-                                <div >{element['projectdescription']}</div>
-                                <div style={{display:'flex'}}>{element['projectlibraries'].map((e,index)=>{ return <div style={{border:"2px solid green",fontSize:'0.7rem',fontWeight:'510' ,margin:'3px',padding:'3px'}} key={index}>{e}</div> })}</div>
-                                <div style={{display:'flex'}}>
-                                    <div style={{margin:'0 5px 0 5px'}}><a target="_blank" rel="noreferrer" href='https://github.com/KowshikEPatel/urlshortnerfe'><i className="fab fa-github"></i></a></div>
-                                    <div style={{margin:'0 5px 0 5px'}}><a target="_blank" rel="noreferrer" href='https://friendly-feynman-57301c.netlify.app'><i className="fas fa-link"></i></a></div>
-                                </div>
-                            </div>
-                })}
+                 <div>
+                    <div className='cardtitle'>{projects[0]['projectname']}</div>
+                    <div >{projects[0]['projectdescription']}</div>
+                    <div style={{display:'flex'}}>{projects[0]['projectlibraries'].map((e,index)=>{ return <div style={{border:"2px solid green",fontSize:'0.7rem',fontWeight:'510' ,margin:'3px',padding:'3px'}} key={index}>{e}</div> })}</div>
+                    <div style={{display:'flex'}}>
+                        <div style={{margin:'0 5px 0 5px'}}><a target="_blank" rel="noreferrer" href='https://github.com/KowshikEPatel/urlshortnerfe'><i className="fab fa-github"></i></a></div>
+                        <div style={{margin:'0 5px 0 5px'}}><a target="_blank" rel="noreferrer" href='https://friendly-feynman-57301c.netlify.app'><i className="fas fa-link"></i></a></div>
+                    </div>
+                </div>
+                
                   <a className='otherprojects' rel="noreferrer" targer='_blank' href='https://github.com/KowshikEPatel?tab=repositories'> Check out my other projects on github</a>
             </div>
             
